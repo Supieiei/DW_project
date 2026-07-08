a2add827d1f624f208e37617ac3050900961b188
Supieiei <baipor398@gmail.com>
2026-07-08 03:01:11 +0000

    update

---
+++ .venv/bacis_command.sh
@@ -0,0 +1,8 @@
+# add  all change to the staging area
+git add .
+# commit the changes with a message
+git commit -m "Your commit message here"
+# push the changes to the remote repository
+git push origin <remote-branch-name> #ให้เปลี่ยน <remote-branch-name> เป็นชื่อ branch ที่ต้องการ push
+# switch to a different branch
+git checkout <branch-name> #ให้เปลี่ยน <branch-name> เป็นชื่อ branch ที่ต้องการ switch ไป
